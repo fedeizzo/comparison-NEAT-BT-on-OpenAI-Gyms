b34a96d418c7c226a957dc14a8e0819511be3010
Federico Izzo <federico.izzo99@gmail.com>
2022-05-05 20:27:49 +0200

    update requirements and nix shell

--- shell.nix
+++ shell.nix
@@ -13,8 +13,20 @@ let
       pypiDataRev = "43f5b07a0b1614ee80723b2ad2f8e29a7b246353";
       pypiDataSha256 = "sha256:0psv5w679bgc90ga13m3pz518sw4f35by61gv7c64yl409p70rf9";
     };
+  requirements =
+    (builtins.replaceStrings [ "git+https://github.com/fedeizzo/neat-python.git" ] [ "" ] (builtins.readFile ./requirements.txt));
+  neat = pkgs.python39Packages.buildPythonPackage {
+    pname = "neat-python";
+    version = "0.93";
+    src = pkgs.fetchFromGitHub {
+      owner = "fedeizzo";
+      repo = "neat-python";
+      rev = "master";
+      sha256 = "sha256-yviV0+NxXTS3o+1ckEg1vQK26q4u3z0Z2L//+/ffHTc=";
+    };
+  };
   python-mach = mach-nix.mkPython {
-    requirements = builtins.readFile ./requirements.txt;
+    requirements = requirements;
     providers = {
       _defualt = "sdist";
     };
@@ -30,7 +42,7 @@ pkgs.mkShell {
     python39Packages.jupyter_console
     python39Packages.jupyter-client
     python39Packages.pyppeteer
-  ] ++ [ python-mach ];
+  ] ++ [ python-mach neat ];
   CHROMIUM_EXECUTABLE_DERK = "$HOME/.nix-profile/bin/google-chrome-stable";
   LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath [pkgs.stdenv.cc.cc]}";
   shellHook = ''

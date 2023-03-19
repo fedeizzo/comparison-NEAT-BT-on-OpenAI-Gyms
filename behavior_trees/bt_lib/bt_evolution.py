c46a70450d0544250e4a2082f57e825658096ed4
Michele Yin <mickyin3@gmail.com>
2023-03-19 13:00:21 +0100

    current lander EA

--- behavior_trees/bt_lib/bt_evolution.py
+++ behavior_trees/bt_lib/bt_evolution.py
@@ -1,4 +1,5 @@
 import os, sys
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from bt_lib.behavior_tree import BehaviorTree
 from bt_lib.composite_nodes import CompositeNode
@@ -7,6 +8,8 @@ from bt_lib.condition_nodes import ConditionNode
 import gymnasium as gym
 import numpy as np
 import random
+import wandb
+from tqdm import tqdm
 
 " cleaned up version of the code"
 
@@ -19,15 +22,35 @@ class BehaviorTreeEvolution:
         crossover_rate: float,
         tournament_size: int,
         elitism_rate: float,
+        tree_size_penalty: float = 0.0,
+        number_generations: int = 100,
+        episodes_number: int = 1,
+        seed=42,
+        save_every=10,
+        folder_path="",
+        train=True,
     ):
         self.population_size = population_size
         self.mutation_rate = mutation_rate
         self.crossover_rate = crossover_rate
         self.elitism_rate = elitism_rate
-        self.population:list[BehaviorTree] = []
-        self.best_tree:BehaviorTree = None
+        self.population: list[BehaviorTree] = []
+        self.best_tree: BehaviorTree = None
         self.tournament_size = tournament_size
-        self.best_tree = None
+        self.tree_size_penalty = tree_size_penalty
+        self.number_generations = number_generations
+        self.episodes_number = episodes_number
+        self.seed = seed
+        self.save_every = save_every
+        self.folder_path = folder_path
+        self.train = train
+        if train:
+            wandb.init(
+                # set the wandb project where this run will be logged
+                project="Lander",
+                # track hyperparameters and run metadata
+                config=self.__dict__,
+            )
 
     def initialize_population(
         self,
@@ -46,11 +69,13 @@ class BehaviorTreeEvolution:
             )
             self.population.append(tree)
         self.best_tree = self.population[0]
-    @staticmethod
-    def evaluate_individual(individual: BehaviorTree, episodes_number : int, env: gym.Env):
+
+    def evaluate_individual(
+        self, individual: BehaviorTree, episodes_number: int, env: gym.Env
+    ):
         fitness = 0
         for _ in range(episodes_number):
-            observation, info = env.reset()
+            observation, info = env.reset(seed=self.seed)
             terminated = False
             while not terminated:
                 state, action = individual.tick(observation)
@@ -62,42 +87,86 @@ class BehaviorTreeEvolution:
                 if terminated or truncated:
                     terminated = True
                 fitness += reward
-        individual.fitness = fitness/episodes_number
-    
-    def evaluate_population(self, episodes_number : int, env: gym.Env) -> None:
+        individual.fitness = (
+            fitness / episodes_number
+            - individual.get_size()[0] * self.tree_size_penalty
+            - individual.get_size()[1] * self.tree_size_penalty
+        )
+
+    def evaluate_population(self, episodes_number: int, env: gym.Env) -> None:
         for individual in self.population:
-            BehaviorTreeEvolution.evaluate_individual(individual, episodes_number, env)
+            self.evaluate_individual(individual, episodes_number, env)
             if individual.fitness > self.best_tree.fitness:
                 self.best_tree = individual
 
-    def select_individual(self,tournament_size : int = 5) -> BehaviorTree:
-        '''
+    def select_individual(self, tournament_size: int = 5) -> BehaviorTree:
+        """
         Selects an individual from the population using tournament selection.
-        '''
+        """
         tournament = random.choices(self.population, k=tournament_size)
         return max(tournament, key=lambda x: x.fitness)
-        
-    def evolve_population(self, episodes_number : int, env: gym.Env) -> list[BehaviorTree]:
+
+    def evolve_population(
+        self, episodes_number: int, env: gym.Env
+    ) -> list[BehaviorTree]:
         self.evaluate_population(episodes_number, env)
         new_population = []
-        sorted_pop_fitness = sorted(self.population, key=lambda x: x.fitness, reverse=True)
-        new_population.extend(sorted_pop_fitness[:int(self.population_size * self.elitism_rate)])
+        sorted_pop_fitness = sorted(
+            self.population, key=lambda x: x.fitness, reverse=True
+        )
+        new_population.extend(
+            sorted_pop_fitness[: int(self.population_size * self.elitism_rate)]
+        )
         while len(new_population) < self.population_size:
-            parent1:BehaviorTree = self.select_individual()
-            parent2:BehaviorTree = self.select_individual()
-            # print(parent1.fitness, parent2.fitness)
-            # print(parent1, parent2)
-            child:BehaviorTree = parent1.recombination(parent2, self.crossover_rate)
-            # print(child)
-            child.mutate(self.mutation_rate, False)
-            # print(child)
+            parent1: BehaviorTree = self.select_individual()
+            parent2: BehaviorTree = self.select_individual()
+            child: BehaviorTree = parent1.recombination(parent2, self.crossover_rate)
+            child.mutate(self.mutation_rate, True)
             # ignore non valid mutations
             if isinstance(child.root, CompositeNode):
                 new_population.append(child)
         self.population = new_population
-        
+
+    def evalutate_folder(
+        self,
+        action_node_classes: list[type[ActionNode]],
+        condition_node_classes: list[type[ConditionNode]],
+        composite_node_classes: list[type[CompositeNode]],
+        env: gym.Env,
+    ):
+        files = os.listdir(self.folder_path)
+        files = sorted(files, key=lambda x: int(x.split("_")[-1].split(".")[0]))
+        for file in files:
+            bt = BehaviorTree.from_json(
+                os.path.join(self.folder_path, file),
+                action_node_classes,
+                condition_node_classes,
+                composite_node_classes,
+            )
+            self.evaluate_individual(bt, self.episodes_number, env)
+
+    def evolutionary_algorithm(self, env: gym.Env) -> BehaviorTree:
+        pbar = tqdm(range(self.number_generations))
+        pbar.set_description("Evolution progress")
+        for i in pbar:
+            self.evolve_population(
+                self.episodes_number,
+                env,
+            )
+            wandb.log({"best_fitness": self.best_tree.fitness})
+            if i % self.save_every == 0:
+                self.best_tree.to_json(
+                    os.path.join(self.folder_path, f"best_tree_generation_{i}.json")
+                )
+
+    def __del__(self):
+        if self.train:
+            wandb.finish()
+            self.best_tree.to_json(os.path.join(self.folder_path, "best_tree.json"))
+
+
 if __name__ == "__main__":
-    
+
     bt = BehaviorTreeEvolution(10, 0.1, 0.7, 5, 0.2)
     from bt_lib.action_nodes import ActionNode
     from bt_lib.condition_nodes import ConditionNode
@@ -105,7 +174,10 @@ if __name__ == "__main__":
     from lunar_lander.action_nodes import action_node_classes
     from lunar_lander.condition_nodes import condition_node_classes
     from bt_lib.composite_nodes import composite_node_classes
-    bt.initialize_population(action_node_classes, condition_node_classes, composite_node_classes, 5)
+
+    bt.initialize_population(
+        action_node_classes, condition_node_classes, composite_node_classes, 5
+    )
     env = gym.make("LunarLander-v2")
     bt.evaluate_population(10, env)
     print(bt.best_tree.fitness)
--- behavior_trees/lander_bt_run.py
+++ behavior_trees/lander_bt_run.py
@@ -5,6 +5,7 @@ from argparse import ArgumentParser
 from bt_lib.behavior_node import BehaviorStates
 from bt_lib.behavior_tree import BehaviorTree
 from tqdm import tqdm
+
 # from bt_lib.behavior_tree_evolution import BehaviorTreeEvolution
 from bt_lib.composite_nodes import CompositeNode, composite_node_classes
 from lunar_lander.action_nodes import action_node_classes
@@ -20,6 +21,12 @@ def main_lander(lander_config):
             lander_config["bt_config"]["crossover_rate"],
             lander_config["bt_config"]["tournament_size"],
             lander_config["bt_config"]["elitism_rate"],
+            lander_config["bt_config"]["tree_size_penalty"],
+            lander_config["bt_config"]["number_generations"],
+            lander_config["bt_config"]["episodes_number"],
+            lander_config["bt_config"]["seed"],
+            lander_config["bt_config"]["save_every"],
+            lander_config["game"]["folder_path"],
         )
 
         env = gym.make("LunarLander-v2")
@@ -31,25 +38,26 @@ def main_lander(lander_config):
             lander_config["bt_config"]["max_depth"],
         )
         # create progress bar
-        pbar = tqdm(range(lander_config["bt_config"]["number_generations"]))
-        pbar.set_description("Evolution progress")
-        for _ in pbar:
-            bt_evolution.evolve_population(
-                lander_config["bt_config"]["episodes_number"],
-                env,
-            )
+        bt_evolution.evolutionary_algorithm(env)
         env.close()
-        bt_evolution.best_tree.to_json(lander_config["game"]["bt_best_player_name"])
     else:
         env = gym.make("LunarLander-v2", render_mode="human")
-        bt = BehaviorTree.from_json(
-            lander_config["game"]["bt_best_player_name"],
-            action_node_classes,
-            condition_node_classes,
-            composite_node_classes,
+        bt_evolution = BehaviorTreeEvolution(
+            lander_config["bt_config"]["population_size"],
+            lander_config["bt_config"]["mutation_rate"],
+            lander_config["bt_config"]["crossover_rate"],
+            lander_config["bt_config"]["tournament_size"],
+            lander_config["bt_config"]["elitism_rate"],
+            lander_config["bt_config"]["tree_size_penalty"],
+            lander_config["bt_config"]["number_generations"],
+            lander_config["bt_config"]["episodes_number"],
+            lander_config["bt_config"]["seed"],
+            lander_config["bt_config"]["save_every"],
+            lander_config["game"]["folder_path"],
+            False,
         )
-        BehaviorTreeEvolution.evaluate_individual(
-            bt, lander_config["bt_config"]["episodes_number"], env
+        bt_evolution.evalutate_folder(
+            action_node_classes, condition_node_classes, composite_node_classes, env
         )
 
 
